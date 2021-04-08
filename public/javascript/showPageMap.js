0c96a796e66f761b87969783c77de17dfeb54b41
Marvin Machitar <machiboy10@gmail.com>
2021-04-08 17:24:52 +0800

    fix bug in map

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,13 +1,14 @@
 
 
-//alert(campground.geometry.coordinates);
 mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 8 // starting zoom
 });
+
+
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
