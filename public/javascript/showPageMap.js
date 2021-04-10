00a946ba5fcfde019ebeeb139d868cd29bc3bf77
Marvin Machitar <machiboy10@gmail.com>
2021-04-10 08:01:33 +0800

    add design to map

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -3,13 +3,23 @@
 mapboxgl.accessToken = mapToken;
 let map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/streets-v10', // style URLhttps://docs.mapbox.com/api/maps/styles/
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 8 // starting zoom
 });
 
 
 
-new mapboxgl.Marker()
+new mapboxgl.Marker({color: '#FF0000'})
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({
+            offset: 25,
+            
+            
+        })
+        .setHTML(
+            `<h3 style="color: #0000ff ">${campground.title}</h3><p>${campground.location}</p>`
+        )
+    )
     .addTo(map)
